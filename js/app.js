e300d1e841400b8f6003321becd92aaed26318c4
Filipe Frozza <filipefrozza@hotmail.com>
2018-01-19 14:05:52 -0200

    Início

---
+++ js/app.js
@@ -0,0 +1,116 @@
+angular.module('angularfirebase', ['ngRoute', 'ngCookies', 'firebase']);
+
+angular.module('angularfirebase')
+	.config(function($routeProvider, $locationProvider){
+		$locationProvider.hashPrefix('');
+		$routeProvider
+			.when('/', {
+	            templateUrl : 'view/home.html',
+	            controller: 'home'
+	        })
+			.when('/teste', {
+				templateUrl: 'view/teste.html',
+				controller: 'teste'
+			})
+			.when('/produtos', {
+				templateUrl: 'view/produtos.html',
+				controller: 'produto'
+			})
+			.when('/login', {
+				templateUrl: 'view/login.html',
+				controller: 'login'
+			})
+			.when('/cadastrar', {
+				templateUrl: 'view/cadastrar.html',
+				controller: 'login'
+			})
+			.otherwise({redirectTo: '/'});
+		$locationProvider.html5Mode({
+		  enabled: true,
+		  requireBase: false
+		});
+	})
+	.controller('produto', function($scope, $firebaseArray, $firebaseAuth){
+		$scope.url = window.location.origin+(window.location.origin=='http://filipefrozza'?'/angular-route-firebase':'');
+		$scope.auth = $firebaseAuth();
+
+		$scope.user = $scope.auth.$getAuth();
+
+		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
+	      	$scope.user = firebaseUser;
+
+			if(!$scope.user) return;
+
+			var ref = firebase.database().ref();
+
+			var refProduto = ref.child('produto');
+		
+			// var syncObject = $firebaseObject(ref);
+
+			PRODUTOS = $firebaseArray(refProduto);
+
+			$scope.produtos = PRODUTOS;
+	    });
+	})
+	.controller('home', function($scope, $firebaseAuth){
+		$scope.url = window.location.origin+(window.location.origin=='http://filipefrozza'?'/angular-route-firebase':'');
+		$scope.auth = $firebaseAuth();
+
+		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
+	      $scope.user = firebaseUser;
+	    });
+
+		$scope.user = $scope.auth.$getAuth();
+	})
+	.controller('login', function($scope, $cookies, $firebaseAuth){
+		$scope.url = window.location.origin+(window.location.origin=='http://filipefrozza'?'/angular-route-firebase':'');
+		$scope.auth = $firebaseAuth();
+
+		$scope.logar = function(user) {
+			$scope.auth.$signInWithEmailAndPassword(
+				user.email, 
+				user.senha
+			).then(function(user){
+				alert('logou como :', user.uid);
+				$cookies.putObject('user', user);
+				document.location.href = window.location.origin+(window.location.origin=='http://filipefrozza'?'/angular-route-firebase':'');
+			}, function(error){
+				if(error == 'INVALID_EMAIL') {
+					console.log('Email invalido ou não cadastrado');
+				}else if(error == 'INVALID_PASSWORD') {
+					console.log('Senha inválida');
+				}else{
+					console.log(error);
+				}
+			});
+		};
+	})
+	.controller('nav', function($scope, $firebaseAuth){
+		$scope.auth = $firebaseAuth();
+		$scope.logout = function(){
+			$scope.auth.$signOut();
+			window.location.reload();
+		}
+	})
+	.directive('appHeader', function(){
+		return {
+			restrict: 'AE',
+			templateUrl: 'include/header.html'
+		}
+	})
+	.directive('appNav', function(){
+		return {
+			restrict: 'AE',
+			scope: true,
+			templateUrl: 'include/nav.html',
+			controller: 'nav'
+		}
+	})
+	.directive('appFooter', function(){
+		return {
+			restrict: 'AE',
+			templateUrl: 'include/footer.html'
+		}
+	});
+
+	
