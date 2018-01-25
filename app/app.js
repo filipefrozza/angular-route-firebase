c379b6c31a480f92d26d382274d497db7f73b7ee
Filipe Frozza <filipefrozza@hotmail.com>
2018-01-25 10:47:48 -0200

    Perfil + controllador main
    Agora no inicio aparece o nome buscando do Perfil no firebase. Também mudei o controller home para main, tentar usálo como padrão

--- app/app.js
+++ app/app.js
@@ -7,8 +7,7 @@ angular.module(APP)
 		$locationProvider.hashPrefix('');
 		$routeProvider
 			.when('/', {
-	            templateUrl : 'app/index.html',
-	            controller: 'home'
+	            templateUrl : 'app/index.html'
 	        })
 			.when('/login', {
 				templateUrl: 'app/pagina/conta/logar.html',
@@ -24,13 +23,16 @@ angular.module(APP)
 		  requireBase: false
 		});
 	})
-	.controller('home', function($scope, $firebaseAuth){
+	.controller('main', function($scope, $firebaseAuth, $firebaseObject){
 		$scope.url = window.location.origin+(window.location.origin=='http://localhost'?'/angular-route-firebase':'');
 		$scope.auth = $firebaseAuth();
 
 		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
-	      $scope.user = firebaseUser;
-	    });
+	      	$scope.user = firebaseUser;
 
-		$scope.user = $scope.auth.$getAuth();
+	      	var ref = firebase.database().ref();
+		    var perfil = $firebaseObject(ref.child('perfil').child($scope.user.uid));
+
+		    $scope.perfil = perfil;
+	    });
 	});
