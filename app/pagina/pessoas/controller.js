83df09d03d20b2293476b7fe5330d70c2d5592e1
Filipe Frozza <filipefrozza@hotmail.com>
2018-02-01 12:59:13 -0200

    só tirando comentários e etc

--- app/pagina/pessoas/controller.js
+++ app/pagina/pessoas/controller.js
@@ -10,32 +10,17 @@ angular.module(APP)
 		};
 
 		$scope.adicionarPessoa = function(pessoa){
-
 			var novaPessoa = angular.copy(pessoa);
 			delete novaPessoa.$id;
 			delete novaPessoa.$priority;
 			delete novaPessoa.$$hashKey;
-			// novaPessoa.key = pessoa.uid;
 
 			console.log(novaPessoa);
 
 			var ref = firebase.database().ref();
 			ref.child('amigos').child($scope.user.uid).child(pessoa.uid).set(novaPessoa);
 
-		    // for(atributo in pessoa){
-		    // 	amigo[atributo] = pessoa[atributo];
-		    // }
-		    // amigo[pessoa.uid] = pessoa;
-		    // amigo.key = pessoa.uid;
-
-
 			console.log(amigo);
-
-		    // amigo.$save().then(function(ref){
-		    // 	console.log(ref.key === amigo.$id);
-		    // }, function(e){
-		    // 	console.log(e);
-		    // });
 		};
 
 		authenticate.getAuth($firebaseAuth, callback);		
